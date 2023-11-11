b52b5d8711b8ab817fb1cbe78d1e597adc3b60d3
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2023-11-11 16:10:29 +0200

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,25 +1,24 @@
-// #ifdef GLOBAL_TEST_ENVIRONMENT
+#ifdef GLOBAL_TEST_ENVIRONMENT
 
-// #include <Arduino.h>
-// #include <gtest/gtest.h>
+#include <Arduino.h>
+#include <gtest/gtest.h>
 
-// void setup()
-// {
-//     Serial.begin(115200);
-//     ::testing::InitGoogleTest();
-// }
+void setup()
+{
+    Serial.begin(115200);
+    ::testing::InitGoogleTest();
+}
 
-// void loop()
-// {
-//     if (RUN_ALL_TESTS())
-//         ;
+void loop()
+{
+    if (RUN_ALL_TESTS())
+        ;
 
-//     delay(1000);
+    delay(1000);
 
-//     Serial.println("-----------------------------------Finished all tests!-----------------------------------");
+    Serial.println("-----------------------------------Finished all tests!-----------------------------------");
 
-//     delay(10000);
-// }
+    delay(10000);
+}
 
-// #endif // GLOBAL_TEST_ENVIRONMENT
-
+#endif // GLOBAL_TEST_ENVIRONMENT
